9a8f72b8560c6a8c499485681419c63bccfb852f
William Lennie Wells <ww347@revenant.maths.cam.ac.uk>
2024-02-29 11:25:46 +0000

    generated remaining pboot plots

--- expmts/input_scripts/parametric_bootstrap.py
+++ expmts/input_scripts/parametric_bootstrap.py
@@ -6,12 +6,13 @@ from src.algos import get_pens
 from src.scaffold.synthetic import load_pboot_mvn
 from src.scaffold.wrappers import get_cv_obj_from_data, compute_everything
 from src.plotting.oracle_panel import panel_plot
+from src.plotting.comparison import row_plot3, stab_row_plot
 
 from src.scaffold.io_preferences import save_mplib
 
 # the three commands to play with
 dataset = 'microbiome'
-regn = 'suo_ridge' # for parametric bootstrap; options 'gglasso', 'ridge', 'suo_ridge'
+regn = 'gglasso' # for parametric bootstrap; options 'gglasso', 'ridge', 'suo_ridge'
 param_choice = 'cv' # how to choose the penalty
 mode = 'run' # 'run' or 'debug'
 
@@ -38,11 +39,19 @@ mvn = load_pboot_mvn(dataset, regn, param_choice)
 data = mvn.gen_data(rs,n)
 print(data.folder_detail)
 
-for algo in algos:
-    cv_obj = get_cv_obj_from_data(data,algo)
-    compute_everything(cv_obj, get_pens(algo, data.n, mode))
+recompute = True
+if recompute:
+    for algo in algos:
+        cv_obj = get_cv_obj_from_data(data,algo)
+        compute_everything(cv_obj, get_pens(algo, data.n, mode))
 
-fig_panel = panel_plot(data)
-suffix = '_debug' if mode == 'debug' else ''
-save_mplib(fig_panel, f'pboot_panel_{abbrev[dataset]}_{abbrev[regn]}_{param_choice}'+ suffix)
+    fig_panel = panel_plot(data)
+    suffix = '_debug' if mode == 'debug' else ''
+    save_mplib(fig_panel, f'pboot_panel_{abbrev[dataset]}_{abbrev[regn]}_{param_choice}'+ suffix)
 
+
+fig, _, _ = row_plot3(data, algos, lambda x: x**2, [0,2,4], y_label='r2sk(-cv)')
+save_mplib(fig, f'pboot_traj_corr_{abbrev[dataset]}_{abbrev[regn]}_{param_choice}')
+
+fig, _ = stab_row_plot(data, algos, ['vt_U', 'wt_U'], [1,3,5],)
+save_mplib(fig, f'pboot_traj_stab_{abbrev[dataset]}_{abbrev[regn]}_{param_choice}')
